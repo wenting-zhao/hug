ab5d385769eca9d9798380c1f74b22b5d85f2dfb
Wenting Zhao <wentingzhao@outlook.com>
2022-06-26 22:50:06 -0400

    Fixed a bug in optimization.

--- main.py
+++ main.py
@@ -200,6 +200,14 @@ def main():
             "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
             "weight_decay": 0.0,
         },
+        {
+            "params": [p for n, p in linear.named_parameters() if not any(nd in n for nd in no_decay)],
+            "weight_decay": args.weight_decay,
+        },
+        {
+            "params": [p for n, p in linear.named_parameters() if any(nd in n for nd in no_decay)],
+            "weight_decay": 0.0,
+        },
     ]
     optim = AdamW(optimizer_grouped_parameters, lr=args.learning_rate)
     loss_fct = nn.CrossEntropyLoss()
