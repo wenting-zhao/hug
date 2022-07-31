dff14a3110f1e1af4008ea01fff4093d7d9d3996
Wenting Zhao <wentingzhao@outlook.com>
2022-07-31 16:16:28 -0400

    Changed default reg value.

--- utils.py
+++ utils.py
@@ -47,7 +47,7 @@ def get_args():
     parser.add_argument('--save_results', action='store_true')
     parser.add_argument('--max_p', action='store_true')
     parser.add_argument('--truncate_paragraph', '-tp', default=0, type=int)
-    parser.add_argument('--reg_coeff', default=0.1, type=float)
+    parser.add_argument('--reg_coeff', default=0, type=float)
     parser.add_argument('--k_distractor', default=1, type=int)
     parser.add_argument('--beam', default=2, type=int)
     parser.add_argument("--batch_size", '-b', default=1, type=int,
